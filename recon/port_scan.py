610ee6c13ad4e5f1f6948d87519abcd361f65d7f
natfalcon7 <falconnatanael@outlook.com>
2026-05-27 18:12:47 +0200

    Phase 2: TCP port scanner

---
+++ recon/port_scan.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+
+def scan_ports(ip, ports):
+	open_ports = []
+
+	for port in ports:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(1)
+
+		result = sock.connect_ex((ip, port))
+
+		if result == 0:
+			open_ports.append(port)
+
+		sock.close()
+		
+	return open_ports
+
+def display_results(ip, open_ports):
+	print(f"\nOpen ports for {ip}")
+	print("----------------------------")
+
+	if open_ports:
+		for port in open_ports:
+			print(f"Port {port}: OPEN")
+	else:
+		print("No open ports found.")
+			
+
+def main():
+	ip = sys.argv[1]
+
+	common_ports = [8022, 88, 443, 8080, 21, 23, 3306]
+
+	open_ports = scan_ports(ip, common_ports)
+	display_results(ip, open_ports)
+
+if __name__ == "__main__":
+	main()
